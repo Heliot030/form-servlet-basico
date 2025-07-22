dbf8273a601358581bdf6b83d25f42139ca72d2f
Heliot Leon <leonheliot@gmail.com>
2025-07-21 22:23:06 -0600

    formulario basico inicial

---
+++ Webapp-Form/src/main/java/apiservlet/webapp/form/FormServlet.java
@@ -0,0 +1,36 @@
+package apiservlet.webapp.form;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/registro")
+public class FormServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        String username = req.getParameter("username");// esto es de name en index para enlazar
+        String password = req.getParameter("password");
+        String email = req.getParameter("email");
+        try (PrintWriter out = resp.getWriter()) {
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>  Resultado Form  </title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Resultado Form </h1>");
+            out.println("<ul>");
+            out.println("          <li> Username :" + username + "</li>");
+            out.println("          <li>  Password :" + password + "</li>");
+            out.println("          <li>  Email  :" + email + "</li>");
+            out.println("</ul>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+}
